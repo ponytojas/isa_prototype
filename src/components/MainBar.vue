348249b767726eb2bcd7bec6bdc8241aff51480a
Daniel Villalobos del Baño <daniel.villalobosdel@gmail.com>
2019-12-29 19:37:01 +0100

    Add login screen

---
+++ src/components/MainBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-app-bar
+      color="success"
+      dark
+      elevate-on-scroll=True
+      relative
+      fixed
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Sistema Integral de Gestión del Arbitraje</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+  </div>
+</template>
