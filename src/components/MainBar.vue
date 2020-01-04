b67bd164b5a4fca375d14bee7973653e2dd60039
Daniel Villalobos del Baño <daniel.villalobosdel@gmail.com>
2020-01-04 20:17:48 +0100

    Starting with new page

---
+++ src/components/MainBar.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-content>
+    <v-app-bar color="success" dark elevate-on-scroll="True" relative fixed>
+      <v-toolbar-title>Sistema Integral de Gestión del Arbitraje</v-toolbar-title>
+      <v-spacer />
+      <v-menu
+        center
+        offset-y
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <v-icon>mdi-account-circle </v-icon>
+          </v-btn>
+        </template>
+
+        <v-list two-line subheader>
+          <v-list-item>
+            <v-list-tile avatar>
+              <v-list-tile-content>
+                <v-list-tile-title>Usuario:  {{ this.username }}</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list-item>
+          <v-list-item @click="logOut">
+            <v-list-tile avatar>
+              <v-list-tile-content>
+                <v-list-tile-title>Cerrar Sesión</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+            </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+  </v-content>
+</template>
+
+<script>
+export default {
+  props: {
+    source: String,
+  },
+  data() {
+    return {
+      level: 0,
+      username: '',
+    };
+  },
+  mounted() {
+    this.username = this.$store.getters.getUsername;
+    this.username = this.username.charAt(0).toUpperCase() + this.username.slice(1);
+    this.level = this.$store.getters.getLevel;
+  },
+  methods: {
+    logOut() {
+      this.$store.dispatch('userLogout');
+    },
+  },
+};
+</script>
---
+++ src/views/Main.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-content>
+      <TopBar />
+    </v-content>
+</template>
+
+<script>
+import TopBar from '../components/MainBar.vue';
+
+export default {
+  name: 'Main',
+
+  components: {
+    TopBar,
+  },
+};
+</script>
