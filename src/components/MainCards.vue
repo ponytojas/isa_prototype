557f9dc7c7123319f2f514ca64d1172df1556ea8
Daniel Villalobos del Baño <daniel.villalobosdel@gmail.com>
2020-01-05 21:05:05 +0100

    New main content

---
+++ src/components/MainCards.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-container class='fill-height' fluid>
+    <v-row align='center' justify='center'>
+      <v-col v-for='card in cards' :key='card.title' :cols='12' sm='6' md='4'>
+        <v-card>
+          <v-img
+            :src='card.src'
+            class='white--text align-end'
+            gradient='to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)'
+            height='200px'
+          >
+            <v-card-title v-text='card.title'></v-card-title>
+          </v-img>
+
+          <v-card-actions>
+            <v-spacer />
+            <v-btn :color='card.color' :dark='card.dark'> Entrar </v-btn>
+            <v-spacer />
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      cards: [
+        {
+          src:
+            'https://images.unsplash.com/photo-1500061228850-950b0dc792c1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80',
+          title: 'Realizar sorteo',
+          color: '#E20D36',
+          dark: true,
+        },
+        {
+          src:
+            'https://images.unsplash.com/photo-1475506631979-72412c606f4d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80',
+          title: 'Datos de equipos',
+          color: '#FFB300',
+          dark: false,
+        },
+        {
+          src:
+            'https://images.unsplash.com/photo-1568495262088-4289581907ca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80',
+          title: 'Datos árbitros',
+          color: '#005744',
+          dark: true,
+        },
+        {
+          src:
+            'https://images.unsplash.com/photo-1562654501-a0ccc0fc3fb1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3289&q=80',
+          title: 'Sancionar equipo',
+          color: '#1B97AD',
+          dark: true,
+        },
+        {
+          src:
+            'https://images.unsplash.com/photo-1510936470381-68e4c0a5e24b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2255&q=80',
+          title: 'Sancionar jugador',
+          color: '#FFAFA2',
+          dark: false,
+        },
+        {
+          src:
+            'https://images.unsplash.com/photo-1536353284924-9220c464e262?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2251&q=80',
+          title: 'Sancionar arbitro',
+          color: '#EABD7D',
+          dark: true,
+        },
+        {
+          src:
+            'https://images.unsplash.com/photo-1468779036391-52341f60b55d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3321&q=80',
+          title: 'Revisar histórico de temporadras',
+          color: '#A5D620',
+          dark: false,
+        },
+      ],
+    };
+  },
+};
+</script>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,17 +1,21 @@
 <template>
     <v-content>
       <TopBar />
+      <v-spacer />
+      <Cards />
     </v-content>
 </template>
 
 <script>
 import TopBar from '../components/MainBar.vue';
+import Cards from '../components/MainCards.vue';
 
 export default {
   name: 'Main',
 
   components: {
     TopBar,
+    Cards,
   },
 };
 </script>
