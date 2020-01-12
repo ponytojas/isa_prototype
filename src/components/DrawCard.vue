640c7db812ac35a5e40943950626fe9d515c5b6b
Daniel Villalobos del Baño <daniel.villalobosdel@gmail.com>
2020-01-12 19:19:56 +0100

    More efficient way

--- src/components/DrawCard.vue
+++ src/components/DrawCard.vue
@@ -3,8 +3,15 @@
     <v-layout row wrap align-center>
       <v-flex xs11 align-center align-content-center>
         <v-card align-center align="center" justify="center">
+          <v-dialog v-model="dialog" hide-overlay persistent width="300">
+            <v-card color="primary" dark>
+              <v-card-text>Por favor espera
+                <v-progress-linear indeterminate color="white" class="mb-0" />
+              </v-card-text>
+            </v-card>
+          </v-dialog>
           <v-card-title>Realizar Sorteo</v-card-title>
-          <v-expansion-panels>
+          <v-expansion-panels  v-model="panel">
             <v-expansion-panel v-for="(value, index) in this.matchesArray" :key="index">
               <v-expansion-panel-header>Jornada {{ index + 1 }}</v-expansion-panel-header>
               <v-expansion-panel-content>
@@ -32,8 +39,9 @@ export default {
       data: Matches,
       matchesArray: [],
       referees: [],
+      panel: [],
       amount: 38,
-      assigned: false,
+      dialog: false,
       headers: [
         {
           text: 'Local',
@@ -64,20 +72,18 @@ export default {
   mounted() {},
   methods: {
     assignation() {
+      this.dialog = true;
       for (let index = 0; index < this.amount; index += 1) {
-        const temp = [];
+        const temp = this.referees.slice();
+        temp.sort(() => 0.5 - Math.random());
         for (let indexFixture = 0; indexFixture < 10; indexFixture += 1) {
-          let referee = this.referees[Math.floor(Math.random() * this.referees.length)].nombre;
-          console.log(referee);
-          console.log(this.matchesArray[index][indexFixture]);
-          while (temp.includes(referee)) {
-            referee = this.referees[Math.floor(Math.random() * this.referees.length)].nombre;
-          }
-          console.log(this.matchesArray[index][indexFixture]);
-          this.matchesArray[index][indexFixture].arbitro = referee;
-          temp.push(referee);
+          const referee = temp.pop(Math.floor(Math.random() * temp.length));
+          this.matchesArray[index][indexFixture].arbitro = null;
+          this.matchesArray[index][indexFixture].arbitro = referee.nombre;
         }
       }
+      this.dialog = false;
+      this.$forceUpdate();
     },
   },
 };
