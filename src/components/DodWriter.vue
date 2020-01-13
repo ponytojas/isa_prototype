92c9bb7162323223ea29ba0576152efcf379a60f
Daniel Villalobos del Baño <daniel.villalobosdel@gmail.com>
2020-01-13 19:25:35 +0100

    Start with the doc

--- src/components/DodWriter.vue
+++ src/components/DodWriter.vue
@@ -1,35 +1,76 @@
 <template>
-  <v-container class="fill-height" grid-list-md text-xs-center align-center>
+  <v-container class='fill-height' grid-list-md text-xs-center align-center>
     <v-layout row wrap align-center>
       <v-flex xs11 align-center align-content-center>
-        <v-card align-center align="center" justify="center">
-            <v-card-title>Redactar Acta</v-card-title>
-            <v-textarea name="input-7-4" label="Acta" style="padding: 20px"
-            outlined flat clear-icon auto-grow/>
+        <v-card align-center align='center' justify='center'>
+          <v-card-title>Redactar Acta</v-card-title>
+          <v-data-table
+            :headers='headers1'
+            :items='matchArray'
+            class='elevation-1'
+            hide-default-footer
+          />
+            <v-dialog v-model='dialog' width='500'>
+              <template v-slot:activator='{ on }'>
+                <v-btn color='primary' style='margin-top: 20px; margin-bottom: 20px' dark v-on='on'>
+                  Añadir Datos
+                </v-btn>
+              </template>
+
+              <v-card>
+                <v-card-title class='headline grey lighten-2' primary-title>
+                  Datos del partido
+                </v-card-title>
+
+                <v-card-text>
+                  <v-container>
+                    <v-row>
+                      <v-col cols='12' sm='6' md='4'>
+                          <v-select  v-model="matchInfo.local" :items="teams" label="Local" />
+                      </v-col>
+                      <v-col cols='12' sm='6' md='4'>
+                        <v-select  v-model="matchInfo.visitante" :items="teams" label="Visitante" />
+                      </v-col>
+                      <v-col cols='12' sm='6' md='4'>
+                        <v-select  v-model="matchInfo.arbitro" :items="referees"
+                        label="Arbitro" />
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
+
+                <v-divider></v-divider>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color='primary' text @click='closeDialog'>
+                    Añadir
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
         </v-card>
       </v-flex>
     </v-layout>
-    <v-snackbar v-model="snackbar"> Asignación realizada con exito
-      <v-btn color="pink" text @click="snackbar = false">Cerrar</v-btn>
-    </v-snackbar>
   </v-container>
 </template>
 <script>
-import Matches from '../assets/data/matches.json';
+import Teams from '../assets/data/teams.json';
 import Referees from '../assets/data/referees.json';
 
 export default {
   data() {
     return {
-      data: Matches,
-      matchesArray: [],
-      referees: [],
-      panel: [],
-      amount: 38,
+      teams: Teams,
+      referees: Referees,
       dialog: false,
-      close: false,
-      snackbar: false,
-      headers: [
+      matchArray: [],
+      matchInfo: {
+        local: '',
+        visitante: '',
+        arbitro: '',
+      },
+      headers1: [
         {
           text: 'Local',
           value: 'local',
@@ -49,31 +90,16 @@ export default {
           sortable: false,
         },
       ],
-      fixtures: [],
     };
   },
   beforeMount() {
-    this.matchesArray = Object.keys(this.data).map((i) => this.data[i]);
-    this.referees = Object.keys(Referees).map((i) => Referees[i]);
+    this.referees = Object.keys(Referees).map((i) => Referees[i].nombre);
   },
   mounted() {},
   methods: {
-    assignation() {
-      this.dialog = true;
-      this.close = true;
-      for (let index = 0; index < this.amount; index += 1) {
-        const temp = this.referees.slice();
-        temp.sort(() => 0.5 - Math.random());
-        for (let indexFixture = 0; indexFixture < 10; indexFixture += 1) {
-          const referee = temp.pop(Math.floor(Math.random() * temp.length));
-          this.matchesArray[index][indexFixture].arbitro = null;
-          this.matchesArray[index][indexFixture].arbitro = referee.nombre;
-        }
-      }
+    closeDialog() {
       this.dialog = false;
-      this.$forceUpdate();
-      this.close = false;
-      this.snackbar = true;
+      this.matchArray.push(this.matchInfo);
     },
   },
 };
